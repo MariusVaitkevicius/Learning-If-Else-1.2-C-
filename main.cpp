b144db0d3c5e2507a9d59d7d989d16f5bb6ce7b4
Marius <80771701+MariusVaitkevicius@users.noreply.github.com>
2022-10-09 18:18:20 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+// B = true, if x = y, otherwise B = false.
+
+int main()
+{
+  int x, y;
+  cout <<"Enter x number: ";
+  cin >> x;
+  cout <<"Enter y number: ";
+  cin >> y;
+  if (x==y) cout <<"answer=true" <<endl;
+  else cout <<"answer=false" <<endl;
+  return 0;
+}
+
+
